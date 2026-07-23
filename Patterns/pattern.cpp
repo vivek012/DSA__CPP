4e6ba3d637a03237a623db35340fb2cc3281513a
vivek012 <vivekk186388@gmail.com>
2026-07-23 17:36:56 +0530

    hashing complete

--- Patterns/pattern.cpp
+++ Patterns/pattern.cpp
@@ -231,8 +231,7 @@ void print15(int n ){
 
 void print17(int n){
     for (int i = 0; i < n; i++)
-    {
-      
+    {  
         // space
         for (int j=0; j<n-i-1; j++){
             cout<< "  ";
